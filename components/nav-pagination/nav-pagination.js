40ab181c6095e5e13e8d0450bc09bb8712f2fbef
codebypaulina <paulinaczeschla@web.de>
2024-08-20 20:19:48 +0200

    pagination and search bar are implemented

---
+++ components/nav-pagination/nav-pagination.js
@@ -0,0 +1,37 @@
+/*
+
+import { fetchCharacters } from "./components/nav-pagination/nav-pagination.js";
+
+import { page } from "../../index.js";
+import { maxPage } from "../../index.js";
+
+import { prevButton } from "../../index.js";
+import { nextButton } from "../../index.js";
+import { pagination } from "../../index.js";
+
+
+prevButton.addEventListener("click", () => {
+  if (page === 1) {
+    return;
+  } else {
+    page--;
+  }
+  fetchCharacters(page);
+});
+
+nextButton.addEventListener("click", () => {
+    if (page === maxPage) {
+      return;
+    } else {
+  page++;
+  consol.log("next Button was clicked");
+    }
+  fetchCharacters(page);
+});
+
+nextButton.addEventListener("click", () => {
+  console.log("button was clicked");
+  fetchCharacters(1);
+});
+
+*/
