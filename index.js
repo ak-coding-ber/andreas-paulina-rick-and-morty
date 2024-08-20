b62b7fad300e43d50162a3a36c545aac738f6640
andreask <a.kupka1@gmx.net>
2024-08-20 16:41:43 +0200

    character info is fetched and cards are added to card container

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,43 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+async function fetchCharacters() {
+  cardContainer.innerHTML = "";
+
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    if (response.ok) {
+      const data = await response.json();
+
+      console.log(data.results);
+      console.log(data.results[1].name);
+      data.results.forEach((character) => {
+        // const cardElement = createCharacterCard(character);
+        // renderElement(cardElement);
+        const newCharacterCard = createCharacterCard(
+          character.name,
+          character.image,
+          character.status,
+          character.type,
+          character.episode.length
+        );
+
+        cardContainer.append(newCharacterCard);
+        // console.log(
+        //   character.name,
+        //   character.image,
+        //   character.status,
+        //   character.type,
+        //   character.episode.length
+        // );
+      });
+    } else {
+      console.log("Bad Response");
+    }
+  } catch (error) {
+    console.error("An Error occurred", error);
+  }
+}
+
+fetchCharacters();
